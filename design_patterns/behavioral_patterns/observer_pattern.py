47660d16453240240dc84ff6274fdb03a06b70e0
Mridul Pant <mridulpant2010@gmail.com>
2024-03-31 17:07:47 +0530

    Update observer_pattern.py
    - included self

--- design_patterns/behavioral_patterns/observer_pattern.py
+++ design_patterns/behavioral_patterns/observer_pattern.py
@@ -1,60 +1,61 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
 from typing import List
-from __future__ import annotations
 from random import randint
 
-class Observer(ABC):
-  @abstractmethod
-  def update(subject:Subject):
-    pass
 
-class ConcreteObserver1(Observer):
-  def update(subject:Subject):
-    return "Concrete Observer1 updates"
-
-class ConcreteObserver2(Observer):
-  def update(subject:Subject):
-    return "Concrete Observer2 updates"
   
 class Subject(ABC):
   @abstractmethod
-  def attach(observer:Observer):
+  def attach(self,observer: Observer):
     pass
   
   @abstractmethod
-  def detach(observer:Observer):
+  def detach(self,observer: Observer):
     pass
   
   @abstractmethod
-  def notify(observer:Observer):
+  def notify(self,observer: Observer):
     pass
   
   @abstractmethod
-  def some_business_logic(observer:Observer):
+  def some_business_logic(self,observer: Observer):
     pass
 
+class Observer(ABC):
+  @abstractmethod
+  def update(self,subject: Subject):
+    pass
 
+class ConcreteObserver1(Observer):
+  def update(self,subject: Subject):
+    return "Concrete Observer1 updates"
+
+class ConcreteObserver2(Observer):
+  def update(self,subject: Subject):
+    return "Concrete Observer2 updates"
+    
 class ConcreteSubject(Subject):
-  _state:int =None #but whose state is this?
+  _state:int =None
   _observers: List[Observer] = []
-  def attach(observer: Observer):
+  def attach(self,observer: Observer):
     self._observers.append(observer)
-  def detach(observer:Observer):
+  def detach(self,observer: Observer):
     self._observers.remove(observer)
-  def notify(observer:Observer):
+  def notify(self,observer: Observer):
     for each_l in self._observers:
       each_l.update(self)
-  def some_business_logic(observer:Observer):
+  def some_business_logic(self,observer: Observer):
     self._state = randint(1,3)
     self.notify(observer)
 
 
 def main():
-  subscriber:Observer = ConcreteObserver1()
-  publisher:Subject  = ConcreteSubject()
+  subscriber: Observer = ConcreteObserver1()
+  publisher: Subject  = ConcreteSubject()
   publisher.attach(subscriber)
   publisher.some_business_logic(subscriber)
-  publisher.detach(subsriber)
+  publisher.detach(subscriber)
     
     
     
